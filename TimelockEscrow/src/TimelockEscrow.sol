bfde533a06dd684314a54587f4b141dd3157dae9
Gy Zhang <kayzh2019@gmail.com>
2023-11-04 13:21:53 -0400

    TImelockEscrow 11/04

---
+++ TimelockEscrow/src/TimelockEscrow.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract TimelockEscrow {
+    address public seller;
+
+    /**
+     * The goal of this exercise is to create a Time lock escrow.
+     * A buyer deposits ether into a contract, and the seller cannot withdraw it until 3 days passes. Before that, the buyer can take it back
+     * Assume the owner is the seller
+     */
+
+    mapping(address => uint256) escrowAmount;
+    mapping(address => uint256) dealTime;
+    uint256 threeDays = 60 * 60 * 24 * 3;
+    
+    constructor() {
+        seller = msg.sender;
+    }
+
+    // creates a buy order between msg.sender and seller
+    /**
+     * escrows msg.value for 3 days which buyer can withdraw at anytime before 3 days but afterwhich only seller can withdraw
+     * should revert if an active escrow still exist or last escrow hasn't been withdrawn
+     */
+    function createBuyOrder() external payable {
+        // your code here
+        require(escrowAmount[msg.sender] == 0,"previous escrow hasn't been withdrawn yet");
+        escrowAmount[msg.sender] = msg.value;
+        dealTime[msg.sender] = block.timestamp;
+    }
+
+    /**
+     * allows seller to withdraw after 3 days of the escrow with @param buyer has passed
+     */
+    function sellerWithdraw(address buyer) external {
+        // your code here
+        require(block.timestamp > dealTime[buyer] + threeDays ,"3 days before seller can withdraw escrow");
+        escrowAmount[buyer]  = 0;
+        (bool success,) = seller.call{value:escrowAmount[buyer]}("");
+        require(success,"seller withdraw failed");
+    }
+
+    /**
+     * allowa buyer to withdraw at anytime before the end of the escrow (3 days)
+     */
+    function buyerWithdraw() external {
+        // your code here
+        require(block.timestamp < dealTime[msg.sender] + threeDays ,"3 days has passed for buy to withdraw escrow");
+        (bool success,) = msg.sender.call{value:escrowAmount[msg.sender]}("");
+        escrowAmount[msg.sender] = 0;
+        require(success,"buyer withdraw failed");
+    }
+
+    // returns the escrowed amount of @param buyer
+    function buyerDeposit(address buyer) external view returns (uint256) {
+        // your code here
+        return escrowAmount[buyer];
+    }
+}
--- week1/src/tutorial.sol
+++ week1/src/tutorial.sol
@@ -133,4 +133,226 @@ contract ExampleContract {
         }
      }
 
+     function useArrayForUint256(uint256[] calldata input) public pure returns(uint256[] memory){
+        return input;
+     }
+
+    function returnFirstElement(uint256[] calldata input) public pure returns(uint256){
+        return input[0];
+     }
+
+    function getArrayLength(uint256[] calldata input) public pure returns(uint256){
+        return input.length;
+     }
+
+    function productOfArray(uint256[] calldata input) public pure returns(uint256){
+        uint256 product = 1;
+        for (uint256 i= 0; i< input.length; i++){
+            product *= input[i];
+        }
+        return product;
+     }
+
+    function productOfArrayFixedSize(uint256[4] calldata input) public pure returns(uint256){
+        uint256 product = 1;
+        for (uint256 i= 0; i< input.length; i++){
+            product *= input[i];
+        }
+        return product;
+     }
+
+     function concatExample(string calldata user) public pure returns( string memory){
+        return string.concat("your username is  ", user);
+     }
+
+     function partOfArray(string calldata str) public pure returns(string memory){
+        //return str[0];      //this will not compile
+        return str;
+        //return str.length;     //this will not compile
+     }
+
+     function fizzBuss(uint256 n) public pure returns (string memory){
+        // if n is divisible by 3, return "fizz"
+        // if n is divisible by 5, return "buzz"
+        // if n is divisible be 3 and 5, return "fizz buzz"
+        // otherwise, return an empty string
+        if ( n % 3 == 0 ){
+            if ( n % 5 == 0){
+                return "fizz buzz";
+            }
+            else {
+                return "fizz";
+            }
+        }
+        else if ( n % 5 == 0) {
+            return "buzz";
+        }
+        else {
+            return "";
+        }
+     }
+
+     function sumArray(uint256[] calldata arr) public pure returns (uint256) {
+        // your code here
+        // arr is a list of unsigned integers
+        // return the sum of them. If the array
+        // is empty, return 0
+        if (arr.length == 0){
+            return 0;
+        }
+        else{
+            uint256 sum;
+            for (uint256 i = 0; i < arr.length; i++){
+                sum += arr[i];
+            }
+            return sum;
+        }
+    }
+
+    /*
+        This exercise assumes you understand how to manipulate Array.
+        1. Function `filterOdd` takes an array of uint256 as argument. 
+        2. Filter and return an array with the odd numbers removed.
+        Note: this is tricky because you cannot allocate a dynamic array in memory, 
+              you need to count the even numbers then declare an array of that size.
+    */
+    function filterOdd(uint256[] memory _arr) public pure returns (uint256[] memory)
+    {
+        // your code here
+        uint256 numOfEven = 0;
+        for (uint256 i = 0; i < _arr.length; i++){
+            if (_arr[i] % 2 == 0){
+                numOfEven++;
+            }
+        }
+        if (numOfEven >0){
+            uint256[] memory evenArr = new uint256[](numOfEven);
+            uint256 j;
+            for (uint256 i = 0; i < _arr.length; i++){
+                if (_arr[i] % 2 == 0){
+                    evenArr[j] = _arr[i];
+                    j ++;
+                }
+            }
+            return evenArr;
+        }
+        else{
+            uint256[] memory result = new uint256[](0);
+            return result;
+        }
+    }
+
+    /**
+     * The goal of this exercise is to return true if the members of "arr" is sorted (in ascending order) or false if its not.
+     */
+    function isSorted(uint256[] calldata arr) public pure returns (bool) {
+        // your code here
+        if (arr.length < 2) {
+            return false;
+        }
+        else{
+            for (uint256 i = 1; i < arr.length; i++) {
+                if (arr[i] < arr[i-1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+     /**
+     * The goal of this exercise is to return the mean of the numbers in "arr"
+     */
+    function mean(uint256[] calldata arr) public pure returns (uint256) {
+        // your code here
+        uint256 N;
+        uint256 sum;
+        for (uint256 i = 0; i < arr.length; i++){
+            N ++;
+            sum += arr[i];
+        }
+        return sum / N;
+    }
+
+    function containsA3(uint256[][] calldata nestedArray) public pure returns (bool) {
+        for ( uint256 i = 0; i < nestedArray.length; i ++){
+            for (uint256 j = 0; j < nestedArray[i].length; j++){
+                if (nestedArray[i][j] == 3){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // ACCEPTED: [[1,2],[3,4],[5,6]]
+    // REJECTED: [[1,2,3],[4,5,6]]
+    function fixedSizeArray(uint256[2][3] calldata nestedArray) public pure returns (uint256) {
+        return nestedArray[2][1]; // just for the sake of compilation
+    }
+
+    uint256[][] public arr;
+
+    function setArr(uint256[][] memory newArr) public {
+        arr = newArr;
+    }
+
+    /**
+     * This function is expected to get the sum of all members of each nested array and finally return the sum of all the nested sums
+     * Example: [[1,2], [3,4]] this should return 1 + 2 + 3 + 4 = 10
+     */
+    function getNestedSum() public view returns (uint256) {
+        // your code here
+        uint256 sum;
+        for ( uint256 i = 0; i < arr.length; i ++){
+            for (uint256 j = 0; j < arr[i].length; j++){
+                sum += arr[i][j];
+            }
+        }
+        return sum;
+    }
+
+
+    /* 
+        This exercise assumes you know how to manipulate nested array.
+        1. This contract checks if TicTacToe board is winning or not.
+        2. Write your code in `isWinning` function that returns true if a board is winning
+           or false if not.
+        3. Board contains 1's and 0's elements and it is also a 3x3 nested array.
+    */
+
+    function isWinning(uint8[3][3] memory board) public pure returns (bool) {
+        // your code here
+        //check 1's
+        if (board[0][0] == 1){
+            if ( (board[1][0] == 1 && board[2][0] == 1) || (board[1][1] == 1 && board[2][2] == 1) ){
+                return true;
+            }
+        }else if (board[0][1] == 1){
+            if (board[1][1] == 1 && board[2][1] == 1) {
+                return true;
+            }
+        }else if (board[0][2] == 1){
+            if (board[1][2] == 1 && board[2][2] == 1) {
+                return true;
+            }
+        }
+
+        //check 0's
+        if (board[0][0] == 0){
+            if ( (board[1][0] == 0 && board[2][0] == 0) || (board[1][1] == 1 && board[2][2] == 0) ){
+                return true;
+            }
+        }else if (board[0][1] == 0){
+            if (board[1][1] == 0 && board[2][1] == 0) {
+                return true;
+            }
+        }else if (board[0][2] == 0){
+            if (board[1][2] == 0 && board[2][2] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
